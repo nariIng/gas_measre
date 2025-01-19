4d35a2707969574419e5749b0431f122f60bb523
nariIng <ing.emmanuela@gmail.com>
2025-01-19 16:07:46 +0300

    first

---
+++ public/js/script.js
@@ -0,0 +1,119 @@
+let poids = [];
+let temps = [];
+let titre = 0;
+
+function dPhGraphique(poids, temps) {
+    const ctx = document.getElementById('titration-curve').getContext('2d');
+
+    const data = {
+        labels: temps,
+        datasets: [{
+            label: 'Masse en fonction du temps',
+            data: poids,
+            borderColor: 'rgb(6, 78, 42)',
+            borderWidth: 2,
+            fill: false,
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Temps (minutes)',
+                        color: '#ffffff'
+                    },
+                    ticks: {
+                        color: '#ffffff'
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.2)'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Masse',
+                        color: '#ffffff'
+                    },
+                    ticks: {
+                        color: '#ffffff'
+                    },
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.2)'
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        color: '#ffffff'
+                    }
+                }
+            }
+        }
+    };
+
+    if (window.dPhGraphiqueInstance) {
+        window.dPhGraphiqueInstance.data.labels = temps;
+        window.dPhGraphiqueInstance.data.datasets[0].data = poids;
+        window.dPhGraphiqueInstance.update();
+    } else {
+        window.dPhGraphiqueInstance = new Chart(ctx, config);
+    }
+}
+
+function start_titre() {
+    titre = 1;
+    fetch(`/start_receiv?text=${titre}`);
+}
+
+function end_titre() {
+    titre = 0;
+    fetch(`/start_receiv?text=${titre}`);
+}
+
+async function fetch_poids() {
+    try {
+        const [response, titre_stat] = await Promise.all ([
+            fetch('/poids'),
+            fetch('/start_send')
+        ]);
+        if (!response.ok) {
+            throw new Error('Erreur réseau lors de la récupération des données');
+        }
+        const data = await response.json();
+        titre = await titre_stat.json();
+        poids = data.poids;
+        temps = data.temps;
+
+        if (titre == 1) {
+            dPhGraphique(poids, temps);
+        }
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données:', error);
+    }
+}
+
+async function sendMessage(event) {
+    event.preventDefault();
+
+    titre = 1;
+    start_titre();
+
+    try {
+        const start = await fetch(`/start`);
+        if (!start.ok) {
+            console.error('Erreur lors de l\'envoi de la vitesse de la pompe:', start.statusText);
+        }
+    } catch (error) {
+        console.error('Erreur lors du commencement:', error);
+    }
+}
+
+setInterval(fetch_poids, 1000);
---
+++ server/server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+const os = require('os');
+
+const app = express();
+const port = 3000;
+
+let poids = [];
+let temps = [];
+let startTime = null;
+let titre = 0;
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json());
+
+const axiosInstance = axios.create({
+    timeout: 20000,
+});
+
+app.get('/start_send', (req, res) => {
+    res.json(titre);
+  })
+
+app.get('/start_receiv', (req, res) => {
+    titre = req.query.text;
+    console.log(titre);
+    if (titre == 1) {
+        poids = [];
+        temps = [];
+        startTime = Date.now();
+    }
+    res.send('Commande reçue');
+});
+
+app.get('/start', async (req, res) => {
+    try {
+        await axiosInstance.get('http://192.168.4.1/start');
+        startTime = Date.now();
+        res.send('Message envoyé');
+    } catch (error) {
+        res.status(500).send('Erreur d\'envoi du message');
+    }
+});
+
+app.get('/poids', async (req, res) => {
+    try {
+        const response = await axiosInstance.get('http://192.168.4.1/poids');
+        let pound = response.data;
+        
+        let elapsedMinutes = 0;
+        if (startTime) {
+            const currentTime = Date.now();
+            elapsedMinutes = Math.floor((currentTime - startTime) / 1000);
+            console.log(temps);
+        }
+        if (poids[poids.length - 1] != pound){
+            poids.push(pound);
+            temps.push(elapsedMinutes);
+        }
+
+        res.json({ poids, temps });
+    } catch (error) {
+        console.error('Erreur lors de la récupération du poids:', error);
+        res.status(500).send('Erreur lors de la récupération du poids');
+    }
+});
+
+function getLocalIp() {
+    const networkInterfaces = os.networkInterfaces();
+    for (const interfaceName in networkInterfaces) {
+        const interfaces = networkInterfaces[interfaceName];
+        for (const iface of interfaces) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return 'localhost';
+}
+
+app.listen(port, () => {
+    const localIp = getLocalIp();
+    console.log(`Serveur Node.js en écoute sur http://localhost:${port}`);
+    console.log(`Serveur Node.js lancé à http://${localIp}:${port}`);
+});
